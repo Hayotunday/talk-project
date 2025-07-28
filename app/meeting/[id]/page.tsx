8b72695155ceb5e967f9d730eca82c63aa8e3b67
Hayotunday <idowudanielayotunde@gmail.com>
2025-07-28 10:38:36 +0100

    change my implementation of useParams

--- app/meeting/[id]/page.tsx
+++ app/meeting/[id]/page.tsx
@@ -1,30 +1,10 @@
-import { getCurrentUser } from "@/lib/actions/auth.action";
-import { Metadata } from "next";
-import { redirect } from "next/navigation";
+"use client";
+
+import { useParams } from "next/navigation";
 import MeetingPage from "@/components/pages/meeting-page";
 
-interface PageProps {
-  params: { id: string };
-  searchParams: { guest: string };
-}
+export default function Page() {
+  const { id } = useParams();
 
-export function generateMetadata({ params: { id } }: PageProps): Metadata {
-  return {
-    title: `Meeting ${id}`,
-  };
-}
-
-export default async function Page({
-  params: { id },
-  searchParams: { guest },
-}: PageProps) {
-  const user = await getCurrentUser();
-
-  const guestMode = guest === "true";
-
-  if (!user && !guestMode) {
-    return redirect("/auth");
-  }
-
-  return <MeetingPage id={id} />;
+  return <MeetingPage id={id! as string} />;
 }
